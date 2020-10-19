51d55758ef362a506297eaa47c7614dc8e798906
arumayara <72974847+arumayara@users.noreply.github.com>
2020-10-19 07:18:19 +0700

    Add files via upload

---
+++ PBO10K-10119905-Latihan13-Relational/src/main/java/PBO10K/NIM10119905/Latihan13/Relational/PBO10K10119905Latihan13.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119905.Latihan13.Relational;
+
+/**
+ *
+ * @author 
+ * NAMA    : Abraham Rumayara
+ * KELAS   : PBO10K
+ * NIM     : 10119905
+ * Deskripsi Program : Program ini menampilkan relational 
+ */
+public class PBO10K10119905Latihan13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bill = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bill == bil2));
+        System.out.println("a != b = " + (bill != bil2));
+        System.out.println("a > b = " + (bill > bil2));
+        System.out.println("a < b = " + (bill < bil2));
+        System.out.println("b >= a = " + (bil2 >= bill));
+        System.out.println("b <= a = " + (bil2 <= bill));
+    }
+    
+}
